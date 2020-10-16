242bd97203144d0264b170e6345f78d739695e48
KookaS <olivier.charrez@hotmail.com>
2020-10-16 18:19:06 +0200

    api

---
+++ components/global/image.ts
@@ -0,0 +1,8 @@
+import Styled from "styled-components";
+
+export const Graph = Styled.img`
+    border-radius: 5px;
+    margin: 20px auto 0 auto;
+    display: block;
+    width: min(98%, 1300px);
+`;
--- services/api.ts
+++ services/api.ts
@@ -1,8 +1,6 @@
-import Router from 'next/router';
-
 export class Api {
     public static host = process.env.NEXT_PUBLIC_API_URL;
-    public authorization: string;
+    // public authorization: string;
     private ctx: any;
 
     public hostName = (): string => {
@@ -11,9 +9,36 @@ export class Api {
 
     public getInitialToken = async (ctx) => {
         this.ctx = ctx;
-    };
+    }
 
     public removeCtx = () => {
         delete this.ctx;
     }
+
+    private get = async (path: string): Promise<Response> => {
+        const requestOptions = {
+            method: 'GET',
+        };
+
+        return await fetch(`${Api.host}/${path}`, requestOptions);
+    };
+
+    private checkBadStatus = (res) => {
+        if (res.status >= 300) {
+            const messages = ["Bad Request", "Server Error"];
+            const index = res.status >= 500;
+
+            throw new Error(`${messages[Number(index)]}: ${res.status}`);
+        }
+    }
+
+    public getImage = async (dbName: string, id: string): Promise<Response> => {
+        try {
+            const res = await this.get(dbName + `/loadImage?id=${id}`);
+            this.checkBadStatus(res);
+            return res;
+        } catch (err) {
+            console.log(err)
+        }
+    };
 }
