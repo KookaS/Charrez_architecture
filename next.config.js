cf620b58c36bbe559ffb57a4a578748509bbfb4f
KookaS <olivier.charrez@hotmail.com>
2020-11-06 20:51:37 +0100

    accueil

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
         return [
             {
                 source: '/',
-                destination: '/acceuil',
+                destination: '/accueil',
                 permanent: true,
             },
         ]
